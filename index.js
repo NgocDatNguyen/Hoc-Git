e9194f7a6b3bb0f45db9af75c808592b466b358f
Ngoc Dat Nguyen <ngocdat99hl@gmail.com>
2021-01-11 20:57:07 +0700

    Them chuc nang cho in gia san pham va doi title

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello");
+let sanPham = prompt("Nhap vao ten san pham: ");
+if (sanPham == "Ga") {
+    console.log("10$");
+} else if (sanPham == "Vit") {
+    console.log("8$");
+} else if (sanPham == "Bo") {
+    console.log("20$");
+}
